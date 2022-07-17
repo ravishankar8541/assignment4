30eb21178fa815fa981a23f9dfdbe7d54fd85d7f
ravishankar8541 <107765864+ravishankar8541@users.noreply.github.com>
2022-07-17 23:53:48 +0530

    Add files via upload

---
+++ Q10.c
@@ -0,0 +1,12 @@
+//Write a program to print a table of 5.
+#include"stdio.h"
+int main()
+{
+    int i,n=5;
+    for(i=1;i<=10;i++){
+        printf("%d X %d = %d",n,i,n*i);
+        printf("\n");
+    }
+    printf("\n");
+    return 0;
+}
