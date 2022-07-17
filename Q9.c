0d52f7f39168acc8edd5df06394180ffd4f4babc
ravishankar8541 <107765864+ravishankar8541@users.noreply.github.com>
2022-07-17 23:53:29 +0530

    Add files via upload

---
+++ Q9.c
@@ -0,0 +1,11 @@
+//Write a program to print cubes of the first 10 natural numbers
+#include"stdio.h"
+int main()
+{
+    for(int i=1;i<=10;i++){
+        printf("cube of %d is %d",i,i*i*i);
+        printf("\n");
+    }
+    printf("\n");
+    return 0;
+}
