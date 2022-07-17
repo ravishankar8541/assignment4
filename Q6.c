a90c98996b43764e83b4630394703d89ecab58e8
ravishankar8541 <107765864+ravishankar8541@users.noreply.github.com>
2022-07-17 23:52:33 +0530

    Add files via upload

---
+++ Q6.c
@@ -0,0 +1,12 @@
+//Write a program to print the first 10 even natural numbers
+#include"stdio.h"
+int main()
+{
+    for(int i=1;i<=20;i++){
+        if(i%2==0){
+            printf("%d ",i);
+        }
+    }
+    printf("\n");
+    return 0;
+}
