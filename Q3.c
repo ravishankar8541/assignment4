19d0c336efc79670b3d840e23c39f25c2f1262f7
ravishankar8541 <107765864+ravishankar8541@users.noreply.github.com>
2022-07-17 23:51:48 +0530

    Add files via upload

---
+++ Q3.c
@@ -0,0 +1,10 @@
+//Write a program to print the first 10 natural numbers in reverse order
+#include"stdio.h"
+int main()
+{
+    for(int i=10;i>=1;i--){
+        printf("%d ",i);
+    }
+    printf("\n");
+    return 0;
+}
